17d0e2aaae0b92345bc49f0e7a070404b4bec5ef
absingh31 <absinghemail@gmail.com>
2017-07-07 13:03:14 +0530

    crawler structure done

--- crawl_bot.py
+++ crawl_bot.py
@@ -1,3 +1,6 @@
+from urllib.request import urlopen
+import tldextract
+
 
 #Importing Stem libraries
 from stem import Signal
@@ -19,4 +22,64 @@ def getaddrinfo(*args):
     return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
 
 socket.getaddrinfo = getaddrinfo
+
+
+class Crawl_bot:
+
+    folder_name, start_link, domain_name, queued_data, crawled_data = '', '', '', '', ''
+    queue = set()
+    data_crawled = set()
+
+    def __init__(self, folder_name, start_link, domain_name):
+        Crawl_bot.folder_name = folder_name
+        Crawl_bot.start_link = start_link
+        Crawl_bot.domain_name = domain_name
+        Crawl_bot.queued_data = Crawl_bot.folder_name + '/queue.txt'
+        Crawl_bot.crawled_data = Crawl_bot.folder_name + '/crawled.txt'
+        self.initiate_directory()
+        self.crawl_page('Spider starts here', Crawl_bot.start_link)
+
+    @staticmethod
+    def initiate_directory():                   # Define and create new directory on the first run
+        create_project_folder(Crawl_bot.folder_name)
+        create_data_files(Crawl_bot.folder_name, Crawl_bot.start_link)
+        Crawl_bot.queue = convert_to_set(Crawl_bot.queued_data)
+        Crawl_bot.data_crawled = convert_to_set(Crawl_bot.crawled_data)
+
+    @staticmethod
+    def crawl_page(thread_name, web_url):      # Fill queue and then update files, also updating user display
+        print(web_url)
+        if web_url not in Crawl_bot.data_crawled:
+            print(thread_name + ' now crawl starts ' + web_url)
+            print('Queue_url ' + str(len(Crawl_bot.queue)) + ' | Crawled_url  ' + str(len(Crawl_bot.data_crawled)))
+            Crawl_bot.add_url_to_queue(Crawl_bot.collect_url(web_url))
+            Crawl_bot.queue.remove(web_url)
+            Crawl_bot.data_crawled.add(web_url)
+            Crawl_bot.update_folder()
+
+    # Converts raw response data into readable information and checks for proper html formatting
+    @staticmethod
+    def collect_url(web_url):
+        html_data_string = ''
+        try:
+            received_response = urlopen(web_url)
+            if 'text/html' in received_response.getheader('Content-Type'):
+                data_bytes = received_response.read()
+                html_data_string = data_bytes.decode("latin-1")
+            link_finder = link_crawler(Crawl_bot.start_link, web_url)
+            link_finder.feed(html_data_string)
+
+##############################################################################################################################################################################################
+#######################################FOR SCRAPPING PURPOSES#################################################################################################################################
+            f = open(Crawl_bot.folder_name + '/' + ((tldextract.extract(web_url)).domain), 'w')
+            f.write(html_data_string)
+            f.close()
+###############################################################################################################################################################################################
+###############################################################################################################################################################################################
+
+        except Exception as e:
+            print(str(e))
+            return set()
+        return link_finder.page_urls()
+
 
